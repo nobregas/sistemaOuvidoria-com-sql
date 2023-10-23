d6f33114f2c664f267e37ac8fca9c323cb7470b1
Gabriel Nobrega <140744513+nobregas@users.noreply.github.com>
2023-10-23 12:06:35 -0300

    Update propriedades.py

--- propriedades.py
+++ propriedades.py
@@ -1,7 +1,7 @@
 dados = {
-    "USER": "root",
-    "PASSWORD": "Derpderp68@",
-    "HOST": "127.0.0.1",
-    "DATABASE": "sistema_ouvidoria"
+    "USER": "UsuarioBD",
+    "PASSWORD": "SenhaBD",
+    "HOST": "Host",
+    "DATABASE": "Nome_DataBase"
 }
 
